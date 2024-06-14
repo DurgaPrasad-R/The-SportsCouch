67442b0c3f98060bde9eb372ef1bb7473f746b5c
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-06-14 13:26:57 +0530

    Change in link

--- server/Team/controllers/storeImg.js
+++ server/Team/controllers/storeImg.js
@@ -1,11 +1,10 @@
 const storeImg = (req, res) => {
-  const apiUrl = import.meta.env.VITE_API_BASE_URL_TEAM;
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
   res.json({
     success: 1,
-    image_url: `${apiUrl}/images/${req.file.filename}`,
+    image_url: `https://the-sportscouch-team.onrender.com/images/${req.file.filename}`,
   });
 };
 
